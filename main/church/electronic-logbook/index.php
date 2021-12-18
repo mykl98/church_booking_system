0c7ce36c1dff8617b5ad701352ce9480a6bcfbfe
mykl98 <michael.skooltech@gmail.com>
2021-12-19 04:52:03 +0800

    1

--- main/church/electronic-logbook/index.php
+++ main/church/electronic-logbook/index.php
@@ -158,7 +158,7 @@
 
     <!-- QR Reader Modal -->
     <div class="modal fade" id="qr-reader-modal" tabindex="-1" role="dialog">
-        <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title text-secondary">Scan QR Code</h5>
